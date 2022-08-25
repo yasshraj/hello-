684c6464bb2313df1f7b3e02e2febd57e7f56889
yashraj purbey <Ypurbey2@huskers.unl.edu>
2022-08-25 17:49:15 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * creater: yashraj purbey
+ *
+ * a program to print my name
+ *
+ */
+#include <std.io>
+#include <stdlin.o>
+
+int main(int argc, char **argv) {
+
+  printf("my name is: yashraj purbey\n");
+
+  return 0;
