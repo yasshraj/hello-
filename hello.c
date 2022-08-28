fc32624548b9bc5ce23513bd26c99e766bf5227a
Nancy Pokharel <npokharel3@huskers.unl.edu>
2022-08-28 18:46:21 +0000

    added name and major

--- hello.c
+++ hello.c
@@ -4,12 +4,17 @@
  * a program to print my name
  *
  */
-#include <std.io>
-#include <stdlin.o>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
 
   printf("my name is: Yashraj purbey\n");
-  printf("my major is: Computer science);
+  printf("my major is: Computer science\n\n");
+
+  printf("nancy pokharel\n");
+  printf("computer science\n");
+
 
   return 0;
+}
